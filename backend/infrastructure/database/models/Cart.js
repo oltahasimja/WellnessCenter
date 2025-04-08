80c078af42c1ea02cf73cf3ceab7655ce05b392b
grajqa <149439049+grajqa@users.noreply.github.com>
2025-04-08 23:21:17 +0200

    Fix Shopping Cart

--- backend/infrastructure/database/models/Cart.js
+++ backend/infrastructure/database/models/Cart.js
@@ -6,7 +6,7 @@ const CartItem = require('./CartItem');
 const Cart = sequelize.define('Cart', {
   userId: {
     type: DataTypes.INTEGER, // Assuming you're using INTEGER for the user ID
-    allowNull: true,
+    allowNull: false,
     references: {
       model: 'Users', // Adjust this to the correct table name for users
       key: 'id',
